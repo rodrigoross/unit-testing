4e010fbd379d8cfcfb02061f21b175666e000082
rodrigoross <rodrigo.ross.comp@gmail.com>
2021-12-18 11:46:55 -0400

    Preparacao de ambiente do curso

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     parser: "babel-eslint",
   },
   rules: {
+    "prettier/prettier": ["error", { tabWidth: 4, enfOfLine: auto }],
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
